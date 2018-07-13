986622fb02a5185d60bf76b1940e6ff3c7fffcc6
Nataliia Nechyporenko <nataliia.nechiporenko@gmail.com>
2018-07-13 17:37:36 +0300

    some refactoring

--- src/js/script.js
+++ src/js/script.js
@@ -2,47 +2,57 @@ $(document).ready(function(){
   let step1 = '';
   let step2 = '';
   let orderData = {
-    sum: 0,
-    term: 0,
+    sum: 1000,
+    term: 12,
     name: '',
     surname: '',
     itn: '',
     city: '',
   };
 
-  let formElements = {
-    "sum": (event) => { checkInputsStep1(event)  },
-    "term": (event) => { checkInputsStep1(event) },
-    "itn": (event) => { checkItn(event) },
+  function renderStep1() {
+    $(".form-wrapper").empty();
+    let step1Template = _.template(document.getElementById('form-step1-template').innerHTML);
+    let formStep1 = step1Template(orderData);
+    $(".form-wrapper").append(formStep1);
   };
 
-  let handleInputsChange = (event) => {
-    let target = event.target.id;
-    for (let key in formElements) {
-         if(target.includes(key)) {
-          formElements[key](event);
-         }
-     };
-  };
+  renderStep1();
 
-  function checkInputsStep1(event) {
-    console.log($(this));
-    if ($(this).val() !== '' ) {
+  $("#sum").on("change", function() {
+    let sum = $(this).val();
+    if (sum !== '' && sum >= 1 && sum <= 10000) {
       $(this).addClass("form__input--ok");
-        // orderData[inputId] = $(this).val();
+      orderData.sum = sum;
       } else {
-        $(this).removeClass("form__input--ok");
-        orderData[inputId] = 0;
-      }
-      console.log(orderData);
-    if (orderData.sum >= 1 
-      && orderData.sum <= 10000
-      && orderData.term >= 1
-      && orderData.term <= 12) {
+      $(this).removeClass("form__input--ok");
+      orderData.sum  = 0;
+    };
+    checkInputsStep1();
+  });
+
+  $("#term").on("change", function() {
+    let term = parseInt($(this).val());
+    if (term !== '' && term >= 1 && term <= 12) {
+      $(this).addClass("form__input--ok");
+      orderData.term = term;
+      } else {
+      $(this).removeClass("form__input--ok");
+      orderData.term = 0;
+    };
+    checkInputsStep1();
+  });
+
+  function checkInputsStep1() {
+    if (orderData.sum !== 0 && orderData.term !== 0) {
         step1 = 'ok';
-        $("#step1-next-btn").addClass("btn--active")
+        $("#step1-next-btn").addClass("btn--active");
+        $("#step1-next-btn").click(renderStep2);
       } else {
         step1 = 'no';
+        console.log(orderData);
+        $("#step1-next-btn").removeClass("btn--active");
+        $("#step1-next-btn").off('click');
       }
   };
 
@@ -53,22 +63,20 @@ function checkItn(event) {
       let minItn = daysFrom1970 - daysIn21Years;
       let itn = $(`#${inputId}`).val();
       let first5CharsItn = itn.slice(0, 4);
-      
       console.log(first5CharsItn);
-
       };
-    
-  
+ 
 // Click handler on the next button on Step1. -> Render Step 2 of form 
-  function handleClick(evt) {
+  function renderStep2(evt) {
     evt.preventDefault();
     $(".form-wrapper").empty();
     let step2Template = _.template(document.getElementById('form-step2-template').innerHTML);
     let formStep2 = step2Template();
     $(".form-wrapper").append(formStep2);
+    $("#step2-back-btn").click(renderStep1);
   }
+});    
 
-//Ivent listeners
-  $("input").on("change", handleInputsChange);
-  $(".btn").click(handleClick);
-});
+function handleClick(evt) {
+  evt.preventDefault();
+}
